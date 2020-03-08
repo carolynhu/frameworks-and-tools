9c0a8ef4b886bfed9f3a3538edf3113b4b7b7d52
carolynhu <carolynprh@google.com>
2020-03-08 14:31:42 -0700

    ginko

---
+++ ginko/books/books_suite_test.go
@@ -0,0 +1,14 @@
+package books_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestBooks(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Books Suite")
+}
+
